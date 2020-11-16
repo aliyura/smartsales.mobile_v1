535545fa30e4dea2176fe205016cc96a05e6dd33
Umar100 <aumar1665@gmail.com>
2020-11-16 16:56:36 +0100

    further restructuring

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -4,25 +4,101 @@ import 'GView.dart';
 import 'model.dart';
 import 'package:shape_of_view/shape_of_view.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   @override
+  DashboardState createState() => DashboardState();
+}
+
+class DashboardState extends State<Dashboard>
+    with SingleTickerProviderStateMixin {
+  bool isCollapsed = true;
+  double screenWidth, screenHeight;
+  Duration duration = const Duration(seconds: 2);
+  AnimationController _controller;
+  Animation<double> _scaleAnimation;
+  Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: duration);
+    _scaleAnimation = Tween<double>(begin: 1, end: 0.6).animate(_controller);
+    _slideAnimation = Tween<Offset>(begin: Offset(-1, 0), end: Offset(0, 0))
+        .animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    screenHeight = size.height;
+    screenWidth = size.width;
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          drawer(context),
-          Gview(),
-          menuButton(context),
-        ],
+      body: Container(
+        height: 1000,
+        child: Stack(
+          children: <Widget>[
+
+            menu(context),
+            // dashboard(context),
+            drawer(context),
+            // Gview(),
+            menuButton(context),
+          ],
+        ),
       ),
     );
   }
 
+  Widget menu(context) {
+    return SlideTransition(
+      position: _slideAnimation,
+      child: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Align(
+              alignment: Alignment.centerLeft,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Dashboard",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Messages",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Utility Biils",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Funds Transfer",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Branches",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ))),
+    );
+  }
+
   Widget menuButton(context) {
-    
-   return( 
-    //  floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      BoomMenu(
+    return (BoomMenu(
+        // FloatingActionButtonLocation.centerDocked,
         animatedIcon: AnimatedIcons.menu_close,
         animatedIconTheme: IconThemeData(size: 22.0),
         //child: Icon(Icons.add),
@@ -112,7 +188,107 @@ class Dashboard extends StatelessWidget {
             onTap: () => print('FOURTH CHILD'),
           )
         ]));
-   }
+  }
+
+  Widget dashboard(context) {
+    return AnimatedPositioned(
+        duration: duration,
+        top: 0,
+        bottom: 0,
+        left: isCollapsed ? 0 : 0.6 * screenWidth,
+        right: isCollapsed ? 0 : -0.4 * screenWidth,
+        child: ScaleTransition(
+          scale: _scaleAnimation,
+          child: Material(
+            animationDuration: duration,
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+            elevation: 8,
+            color: Colors.blue,
+            child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                physics: ClampingScrollPhysics(),
+                child: Container(
+                  padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            InkWell(
+                              child: Icon(
+                                Icons.menu,
+                                color: Colors.white,
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  if (isCollapsed)
+                                    _controller.forward();
+                                  else
+                                    _controller.reverse();
+                                  isCollapsed = !isCollapsed;
+                                });
+                              },
+                            ),
+                            Text(
+                              "My Cards",
+                              style:
+                                  TextStyle(fontSize: 24, color: Colors.white),
+                            ),
+                            Icon(
+                              Icons.settings,
+                              color: Colors.white,
+                            ),
+                          ]),
+                      SizedBox(height: 50),
+                      Container(
+                        height: 200,
+                        child: PageView(
+                          controller: PageController(viewportFraction: 0.8),
+                          scrollDirection: Axis.horizontal,
+                          pageSnapping: true,
+                          children: <Widget>[
+                            Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 8),
+                              color: Colors.redAccent,
+                              width: 100,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 8),
+                              color: Colors.blueAccent,
+                              width: 100,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 8),
+                              color: Colors.greenAccent,
+                              width: 100,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text("Transaction",
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                      ListView.separated(
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text("Windows"),
+                              subtitle: Text("Windows 10"),
+                              trailing: Text("-2900"),
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return Divider(height: 16);
+                          },
+                          itemCount: 10),
+                    ],
+                  ),
+                )),
+          ),
+        ));
+  }
 
   Widget drawer(context) {
     Color textcolor = Colors.white;
